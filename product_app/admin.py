fea04f48e04fa3185acf48cab0b7ff9db4e124f0
Dianas <diana.rytko@mail.ru>
2024-03-18 20:14:23 +0300

    init

--- product_app/admin.py
+++ product_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Notebook, Image
 
-# Register your models here.
+admin.site.register(Notebook)
+admin.site.register(Image)
+
--- product_app/models.py
+++ product_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Notebook(models.Model):
+    '''Ноутбуки'''
+
+    link = models.CharField(max_length=255, unique=True, verbose_name='Ссылка')
+    title = models.CharField(max_length=500, verbose_name='Заголовок')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    description = models.TextField(verbose_name='Описание')
+    producer = models.CharField(max_length=200)
+    diagonal = models.CharField(max_length=100)
+    resolution = models.CharField(max_length=100)
+    os = models.CharField(max_length=100)
+    processor = models.CharField(max_length=100)
+    ram = models.CharField(max_length=100)
+    video_card = models.CharField(max_length=100)
+    hdd_type = models.CharField(max_length=100)
+    hdd_volume = models.CharField(max_length=100)
+    battery = models.CharField(max_length=100)
+    condition = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Ноутбук'
+        verbose_name_plural = 'Ноутбуки'
+        db_table = 'Сводка по ноутбукам'
+
+    def __str__(self):
+        return f'{self.title} | {self.price}'
+
+
+class Image(models.Model):
+    """Изображения"""
+
+    image = models.URLField(max_length=255, unique=True)
+    notebook = models.ForeignKey(Notebook, on_delete=models.CASCADE, related_name='images')
+
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+
+
+    def __str__(self):
+        return self.image
+
+
